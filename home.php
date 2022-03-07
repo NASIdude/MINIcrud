137c60d7b5c9f96dadf6fcc603cb735e18c85da3
Naser <nasidude123@gmail.com>
2022-03-07 13:16:19 +0100

    php

---
+++ home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="minicrud css/MiniCRUD.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Overpass&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <a href="https://github.com/" target="_blank">github</a>
+            <a href="https://www.minecraft.net/nl-nl"  target="_blank">minecraft</a>
+            <a href="https://www.nintendo.nl/#" target="_blank">nintendo</a>
+        </div>
+    </header>
+    <main>
+        <h1>Welkom bij Ling Ling's Pizzeria 🍕.</h1>
+        <div class="begin-stuk">
+            <p>Deze pizza worden stuk voor stuk vers en overheerlijk gebakken recht voor u neus.</p>
+        </div>    
+    </main>
+    <script src="js/alles.js"></script>
+</body>
+</html>
