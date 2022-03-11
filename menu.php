5772c8b2a0b1d43927b6631d7e0a2bec184a9d5e
Naser <nasidude123@gmail.com>
2022-03-11 13:36:37 +0100

    menu

---
+++ menu.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="minicrud css/MiniCRUD.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Overpass&display=swap" rel="stylesheet">
+</head>
+<body>
+<header>
+        <div class="navbar">
+            <a href="https://github.com/" target="_blank">Github</a>
+            <a href="https://www.minecraft.net/nl-nl"  target="_blank">Minecraft</a>
+            <a href="https://www.nintendo.nl/#" target="_blank">Nintendo</a>
+        </div>
+        <div class="begin-stuk">
+            <h1>Welkom bij Ling Ling's Pizzeria 🍕.</h1>
+        </div>
+    </header>
+    <main>
+    <h1>DIng dong</h1>
+    </main>
+    <footer>
+        <div class="footbar">
+            <p>Gemaakt door Naser Jafari ©.</p>
+        </div>
+    </footer>
+    <script src="js/alles.js"></script>
+</body>
+</html>
