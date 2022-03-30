259e86243eac7d99bfff908b1eccd61562a0cf6e
Naser <nasidude123@gmail.com>
2022-03-30 09:21:27 +0200

    includes

---
+++ includes/footer.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="minicrud css/MiniCRUD.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Overpass&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/6d463992be.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <footer>
+        <div class="footbar">
+            <div class="openingstijden">
+                <p>Openingstijden</p>
+                <ul>
+                    <li>Maandag: 17:00 - 21:00</li><br>
+                    <li>Dinsdag: 17:00 - 21:00</li><br>
+                    <li>Woensdag: 17:00 - 21:00</li><br>
+                    <li>Donderdag: 17:00 - 21:00</li><br>
+                    <li>Vrijdag: 15:00 - 23:00</li><br>
+                    <li>Zaterdag: 14:00 - 22:00</li><br>
+                    <li>Zondag: 10:00 - 18:00</li><br>
+                </ul>
+            </div>
+            <div class="social-media">
+                <p>Volg ons op de sociale media.</p>
+                <ul>
+                    <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Instagram</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"  target="_blank">Facebook</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Twitter</a></li>
+                </ul>
+            </div>
+            <div class="adres">
+                <p>Hier kan je ons vinden</p>
+                <ul>
+                    <li>Heyendaalseweg 98</li><br>
+                    <li>6525 AW Nijmegen</li><br>
+                    <li>024 890 4500</li><br>
+                </ul>
+            </div>
+            <div class="map">
+                <iframe src="https://www.bing.com/maps/embed?h=400&w=500&cp=51.827145037032615~5.8690595626831055&lvl=16&typ=d&sty=h&src=SHELL&FORM=MBEDV8" scrolling="no"></iframe>
+                <div style="white-space: nowrap; text-align: center; width: 500px; padding: 6px 0;">
+                    <a id="largeMapLink" target="_blank" href="https://www.bing.com/maps?cp=51.827145037032615~5.8690595626831055&amp;sty=h&amp;lvl=16&amp;FORM=MBEDLD">Grotere kaart weergeven</a> &nbsp; | &nbsp;
+                    <a id="dirMapLink" target="_blank" href="https://www.bing.com/maps/directions?cp=51.827145037032615~5.8690595626831055&amp;sty=h&amp;lvl=16&amp;rtp=~pos.51.827145037032615_5.8690595626831055____&amp;FORM=MBEDLD">Routebeschrijving weergeven</a>
+                </div>
+            </div>
+            <div class="inner-footer flex"></div>
+            <div>
+            <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+            <defs>
+            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                 </defs>
+                    <g class="parallax">
+                        <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(78, 0, 194" />
+                        <use xlink:href="#gentle-wave" x="48" y="2" fill="rgba(60, 103, 227)" />
+                        <use xlink:href="#gentle-wave" x="48" y="4" fill="rgba(93, 220, 255)" />
+                        <use xlink:href="#gentle-wave" x="48" y="6" fill="#fff" />
+                    </g>
+                </svg>
+            </div>
+        </div>
+        <div class="content flex">
+            <p>Design by Naser Jafari ©</p>
+        </div>
+    </footer>
+</body>
+</html>
