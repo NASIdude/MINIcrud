56affffb3ade783c836234788df97dc7097b287a
Naser <nasidude123@gmail.com>
2022-04-21 16:40:06 +0200

    login form
    bezig met login form maken

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+    include_once('includes/session.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="css/MiniCRUD.css">
+</head>
+<body>
+    
+</body>
+</html>
